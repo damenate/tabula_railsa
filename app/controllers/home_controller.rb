6066513c990bc753bd375d4767de7ec2926d87df
Da-Me <damekim1206@gmail.com>
2015-10-07 10:59:32 -0400

    update on challege 10/07

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,20 @@ class HomeController < ApplicationController
 
   def form
     @me_liky = MeLiky.new
-    if request.post?
-    @me_liky = MeLiky.create!(name: params[:name], chocolate: params[:chocolate],
-      rainbows: params[:rainbows], puppies: [:puppies],
-      cold_hard_cash: params[:cold_hard_cash])
-    end
   end
 
   def new
     @me_likies = MeLiky.new.all
   end
 
+  def create
+    if request.post?
+    @me_liky = MeLiky.create!(name: params[:name], chocolate: params[:chocolate],
+      rainbows: params[:rainbows], puppies: [:puppies],
+      cold_hard_cash: params[:cold_hard_cash])
+      redirect_to home_create_path
+    end
+  end
   private def me_liky_params
 
     def me_liky_params
